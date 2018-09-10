cecbefb3637714f258d2f1cc405b333e72a9bfc2
Anton Shramko <antonshramko@yandex.ru>
2018-09-11 01:23:21 +0300

    adding rename-all as kebab-case for properties

--- src/ffi_types/align.rs
+++ src/ffi_types/align.rs
@@ -3,6 +3,7 @@ use internal;
 #[repr(u32)]
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Copy, Clone)]
 #[cfg_attr(feature = "serde_support", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "serde_support", serde(rename_all = "kebab-case"))]
 pub enum Align {
 	Auto = 0,
 	FlexStart = 1,
--- src/ffi_types/display.rs
+++ src/ffi_types/display.rs
@@ -3,6 +3,7 @@ use internal;
 #[repr(u32)]
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Copy, Clone)]
 #[cfg_attr(feature = "serde_support", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "serde_support", serde(rename_all = "kebab-case"))]
 pub enum Display {
 	Flex = 0,
 	None = 1,
